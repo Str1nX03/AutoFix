2e539aad02774f9525a6bf8b3f85aa4bacbcc8d2
ShivSingh-17 <shiva.singh170304@gmail.com>
2026-06-27 23:16:07 +0530

    database column  Modified

--- database/models.py
+++ database/models.py
@@ -10,8 +10,12 @@ class Product(Base):
     __tablename__ = "products"
 
     product_id = Column(Integer, primary_key=True, index=True)
+    product_type = Column(String, nullable=False)
+    product_field = Column(String, nullable=False)    
     name = Column(String, index=True, nullable=False)
     description = Column(Text)
     price = Column(String) # Storing as string for simplicity (e.g., "$50" or "Rs. 5000")
-
+    Product_Webpage_url = Column(String, nullable=False)
+    product_issues = Column(String, nullable=False)
+    
 # In the future, you will add ChatSessions and ChatMessages tables here
