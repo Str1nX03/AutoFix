3effc635cd82ea46b7ee49fe8b90e39082c27d1e
ShivSingh-17 <shiva.singh170304@gmail.com>
2026-06-28 16:34:17 +0530

    Column modified

--- database/models.py
+++ database/models.py
@@ -12,10 +12,10 @@ class Product(Base):
     product_id = Column(Integer, primary_key=True, index=True)
     product_type = Column(String, nullable=False)
     product_field = Column(String, nullable=False)    
-    name = Column(String, index=True, nullable=False)
-    description = Column(Text)
-    price = Column(Integer) 
-    Product_Webpage_url = Column(String, nullable=False)
+    product_name = Column(String, index=True, nullable=False)
+    product_description = Column(Text)
+    product_price = Column(Integer) 
+    product_url = Column(String, nullable=False)
     product_issues = Column(JSON, nullable=False)
 
 # In the future, you will add ChatSessions and ChatMessages tables here
