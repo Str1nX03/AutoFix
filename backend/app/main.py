bad02403a5eaf6217943d634b5f1f75f89d073e8
jagankrishnaus@gmail.com <jagankrishnaus@gmail.com>
2026-07-11 21:27:34 +0530

    main.py updated with allow origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,10 @@ app = FastAPI(title="AutoFix AI Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://autofix-frontend-vln8.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
