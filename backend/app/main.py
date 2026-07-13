720abcae50812974b254ddfbf6b37b9abefa09c5
Str1nX03 <draveen2210@gmail.com>
2026-07-13 15:48:45 +0530

    frontend url integrated

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://auto-fix-sooty.vercel.app/"
+        "https://auto-fix-sooty.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
